d85a930b42f98507876341b93fd140e617ee33d4
Kurtos András <andraskurtos@gmail.com>
2025-02-17 15:55:24 +0100

    base

---
+++ mapgenerator/src/index.tsx
@@ -0,0 +1,15 @@
+import { render } from 'preact';
+
+import preactLogo from './assets/preact.svg';
+import './style.css';
+
+export function App() {
+	return (
+		<div>
+			
+		</div>
+	);
+}
+
+
+render(<App />, document.getElementById('app'));
